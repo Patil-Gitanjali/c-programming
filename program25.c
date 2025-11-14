c5fe395373da1b419a01721363b5205fbf36209b
Patil-Gitanjali <Gitanjalipatil2204@gmail.com>
2025-11-14 09:36:17 +0530

    Add files via upload

---
+++ program25.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+  
+ //static function
+ void Display()
+ {
+    int icnt = 0;
+
+    for(icnt = 1; icnt<=4; icnt++)
+    {
+      printf("Jay Ganesh..\n");
+    }
+ }
+ 
+ // it should be int main
+int main()
+{   
+    Display();
+    
+    return 0;
+}
